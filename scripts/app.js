718a86da9c53685a151bf165142434a9a6588bce
jennifergoldberg <jagoldbe@gmail.com>
2021-07-14 10:55:08 -0400

    added more HTML and JS for the start game method

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@
     - The field will be a layer on top of the main page that puts an opacity on top of the main page
 3. The opacity will be removed and the user will see the sandwich game
     - The user's chosen name will be displayed in the header
-4. The user will "meet" their sandwich as a Tea Sandwich
+4. The user will "meet" their sandwich as a Tea Sandwich and the user can start the game by clicking any button
 5. The user will click one of three buttons to take care of the sandwich by meeting the sandwich's "needs"
     - The Feed button will run the feedMe() that will feed the sandwich and adjust the progress bar for the hunger metrics 
     - The Slice button will run the sliceMe() that will play with the sandwich to prevent boredom and adjust the progress bar for the boredom metrics 
@@ -26,55 +26,67 @@
     */
 
 
-/*
-1. The user opens the site on the browser 
-2. The user see a box with a blank field and is asked to enter a name for their sandwich and press a submit button that says " "
-    - The field will have a max character count of 25
-    - The field will be a layer on top of the main page that puts an opacity on top of the main page 
 
-    1. create a div that will be the box
-    2. put the form on top of the div
-    3. store the user input seperate from the div
-    4. 
-    
-    */
-
-    const nameSando = function(name){
-
-    }
     
     /* 5. The user will click one of three buttons to take care of the sandwich by meeting the sandwich's "needs"
         - The Feed button will run the feedMe() that will feed the sandwich and adjust the progress bar for the hunger metrics 
         - The Slice button will run the sliceMe() that will play with the sandwich to prevent boredom and adjust the progress bar for the boredom metrics 
         - The Ice button will run the iceMe() that will put the sandwich to bed and adjust the progress bar for the sleepiness metrics 
-        -grab button and make jq variable 
-        -create button function
-        -attach button to metric
-        -make metric move up by 1
+
+
+        -create a game object to hold data 
+
+        -select button from dom
+        -create a new method to handle the feedme button
+        -connect the start game method to new method
+
+        -select span from dom and make a variable
+        -create a function for the metric to add +1 for every "click"
         
         */
+
+
+/* 4. The user will "meet" their sandwich as a Tea Sandwich and the game will start
+- create a game object - done
+- add event listener "click" 
+- start method will log the start of the game and start counter
+    -create counter method
+    -attach it to the 
+*/
     
-    const sandoGame = {
-        round: 3,
-        feedScore: 0,
-        icedScore: 0,
-        slicedScore: 0,
+const sandoGame = {
+    round: 1,
+    feedScore: 0,
+    icedScore: 0,
+    slicedScore: 0,
+
+
+/*  startSando(event){
+       // console.log("click");
+        sandoGame.handleFeedMe();
+
+    },*/
+
+    $countFeedClick: $(".feed-count"),
     
-        
-    
+    handleFeedMe(event){
+        const $countFeedClick = $(event.target)
+        if ($countFeedClick.hasClass("feed-count") === true && sandoGame.feedScore <= 10){
+            console.log($countFeedClick.hasClass("feed-count"));
+            sandoGame.feedScore++;
+            }
+        $(".feed-count").text(` ${sandoGame.feedScore}`);
+        },
 
     
-        feedMe(){
-            //const $feedScore = $("#feed-count");
-            //$("#feed") = event.target;
-            //sandoGame.feedScore++;
-            //$feedScore.text(`${sandoGame.score}`);
-            console.log("click");
-        },
-    
-        
     
     
     };
     
-    $("#feed").on('click', sandoGame.feedMe());
+    
+    
+    
+$("#feed").on("click", sandoGame.handleFeedMe);
+$("#ice").on("click", sandoGame.startSando);
+$("#slice").on("click", sandoGame.startSando);
+
